03c954020cd62aa65f08e30b68c28d786c2973e8
Mateusz Janusz <mateuszjanusz95@gmail.com>
2017-10-13 22:25:26 +0100

    more progress, more test files, attempt to copy files

--- sort.js
+++ sort.js
@@ -6,59 +6,118 @@ const testFolder = './test_images/'
 
 module.exports = {
 
-	sortImages(dir) {
+	sortImages(directory) {
 		const _this = this
 
-		if(!dir.charAt(dir.length-1).includes('/')){
-			dir = dir + '/'
-		}
+		const dir = _this.formatDirectoryPath(directory)
 
 		fs.readdirSync(dir).forEach(file => {
 			const is_image = _.endsWith(file, 'jpg') || _.endsWith(file, 'png') || _.endsWith(file, 'jpeg')
 			
 			if(is_image){
-				new ExifImage({ image : dir + file }, function (error, exifData) {
-			        if (error) console.log(error)
-			        else {
-			        	const exif = exifData.exif
-			        	const image_data = exifData.image
+				try {
+					new ExifImage({ image : dir + file }, function (error, exifData) {
+				        if (error) console.log(error)
+				        else {
+				        	const exif = exifData.exif
+				        	const image_data = exifData.image
 
-			        	const image_timespan = exif.DateTimeOriginal || exif.CreateDate || image_data.ModifyDate
+				        	const image_timespan = exif.DateTimeOriginal || exif.CreateDate || image_data.ModifyDate
 
+				        	if(image_timespan){
+				        		const date = moment(image_timespan.split(' ')[0], ['YYYY:MM:DD'])
+						        const year = moment(date, ['YYYY:MM:DD']).format('YYYY')
+						        const month = moment(date).format('MMMM')
 
-				        const date = moment(image_timespan.split(' ')[0], ['YYYY:MM:DD'])
-				        const year = moment(date, ['YYYY:MM:DD']).format('YYYY')
-				        const month = moment(date).format('MMMM')
+						        const destination = _this.getDestination(dir, year, month)
 
-				        const destination = _this.createDirectory(dir, year, month)
+	    			            console.log("this image would go to: ", destination)
+	    			            _this.copyFile(file, destination)
+	    			            
 
-			            console.log("this file should go to: ", destination)
-			        }
-			    })
+				        	} else {
+				        		_this.moveToUnknowns(dir)
+				        	}
+					       
+
+				        }
+				    })
+				} catch(err){
+					console.log(err)
+				}  
+			} else {
+				console.log("not an image: ", file)
 			}
 
 		})
 	},
 
-	createDirectory(dir, year, month) {
-		const new_folder = dir + 'Sorted'
+	formatDirectoryPath(directory){
+		let dir = _.clone(directory)
 
-		if(!fs.existsSync(new_folder)){
-			final_dir = new_folder
-			fs.mkdirSync(new_folder)
+		if(!dir.charAt(dir.length-1).includes('/')){
+			dir = dir + '/'
 		}
 
-		const year_folder = new_folder + '/' + year
-		if(!fs.existsSync(year_folder)){
-			fs.mkdirSync(year_folder)
-		}
+		return dir 
+	},
+ 
+	setupDirectory(directory, name) {
+		const dir = this.formatDirectoryPath(directory)
 
-		const month_folder = year_folder + '/' + month
-		if(!fs.existsSync(month_folder)){
-			fs.mkdirSync(month_folder)
+		const dir_path = dir + name
+
+		if(!fs.existsSync(dir_path)){
+			fs.mkdirSync(dir_path)
 		}
+		return dir_path
+	},
+
+	getDestination(dir, year, month) {
+		const sorted_folder = this.setupDirectory(dir, 'Sorted')
+		const year_folder = this.setupDirectory(sorted_folder, year)
+		const month_folder = this.setupDirectory(year_folder, month)
 
 		return month_folder
+	},
+
+	copyFile(file, destination){
+		destination = this.formatDirectoryPath(destination)
+		destination = destination + file
+
+		if(!fs.existsSync(destination)){
+
+		} else {
+			destination = destination + '_copy'
+		}
+
+	},
+	// function copyFile(source, target, cb) {
+	//   var cbCalled = false;
+
+	//   var rd = fs.createReadStream(source);
+	//   rd.on("error", function(err) {
+	//     done(err);
+	//   });
+	//   var wr = fs.createWriteStream(target);
+	//   wr.on("error", function(err) {
+	//     done(err);
+	//   });
+	//   wr.on("close", function(ex) {
+	//     done();
+	//   });
+	//   rd.pipe(wr);
+
+	//   function done(err) {
+	//     if (!cbCalled) {
+	//       cb(err);
+	//       cbCalled = true;
+	//     }
+	//   }
+	// }
+
+	moveToUnknowns(directory){
+		this.setupDirectory(directory, 'Unknowns')
 	}
 
 }
