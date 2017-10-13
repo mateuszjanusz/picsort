24d1fe704f364f740798b215474dbba962dc4d3e
Mateusz Janusz <mateuszjanusz95@gmail.com>
2017-10-13 13:28:09 +0100

    extract image created date for all image files

--- exif_extractor.js
+++ exif_extractor.js
@@ -1,12 +1,19 @@
-const ExifImage = require('exif').ExifImageg
+const fs = require('fs')
+const ExifImage = require('exif').ExifImage
+const testFolder = './test_images/'
 
-try {
-    new ExifImage({ image : 'test_images/test3.jpg' }, function (error, exifData) {
-        if (error)
-            console.log('Error: '+error.message)
-        else
-            console.log(exifData)
-    });
-} catch (error) {
-    console.log('Error: ' + error.message)
-}
+const file_names = []
+
+fs.readdirSync(testFolder).forEach(file => {
+	new ExifImage({ image : testFolder + file }, function (error, exifData) {
+        if (error) console.log(error)
+        else {
+        	const exif = exifData.exif
+        	const image_data = exifData.image
+
+        	const image_timespan = exif.DateTimeOriginal || exif.CreateDate || image_data.ModifyDate
+        	
+            console.log(image_timespan)
+        }
+    })
+})
