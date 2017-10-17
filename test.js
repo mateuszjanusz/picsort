2790c09f96983f0eef60bcb2d28f88d603bdbf6a
Mateusz Janusz <mateuszjanusz95@gmail.com>
2017-10-17 20:08:26 +0100

    new ver

--- test.js
+++ test.js
@@ -2,7 +2,4 @@ const Picsort = require('./lib/picsort')
 const test_path = './test_images/'
 
 
-
-const path_to_images = process.argv[2] || test_path
-
-Picsort.sortImagesByDate(path_to_images)
+Picsort.sortImagesByMonth(test_path)
