66d0147b10dc1604b57a7daf70c0dba35e8218e6
Mateusz Janusz <mateuszjanusz95@gmail.com>
2017-10-18 22:40:51 +0100

    added travis and some simple tests

--- test.js
+++ test.js
@@ -1,5 +1,32 @@
-const Picsort = require('./lib/picsort')
+const picsort = require('./lib/picsort')
 const test_path = './test_images/'
+const assert = require('assert')
+const chai = require('chai')
+const expect = chai.expect
 
+describe('picsort', function() {
 
-Picsort.sortImagesByMonth(test_path)
+	describe('function exists', function() {
+	    it('should return true when directory exists', function() {
+	     	const result = picsort.exists('test.js')
+	     	expect(result).to.be.true
+	    })
+	    it('should return false when directory does not exists', function() {
+	     	const result = picsort.exists('folder')
+	     	expect(result).to.be.false
+	    })
+	})
+
+	describe('function formatDirectoryPath', function() {
+	    it('should add "/" at the end of the string', function() {
+	     	const result = picsort.formatDirectoryPath('new_dir')
+	     	expect(result).to.be.equal('new_dir/')
+	    })
+	    it('should return original string if it already contains "/"', function() {
+	     	const result = picsort.formatDirectoryPath('new_dir/')
+	     	expect(result).to.be.equal('new_dir/')
+	    })
+	    
+	})
+
+})
